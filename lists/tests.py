0d8fc01896345683fa42d382b531408f05deaf7e
wilfried <wilfried.odorico@gmail.com>
2024-10-04 20:52:30 +0200

    clean test and add urls

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,9 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from .views import home_page
 
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf-8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lists.views import home_page
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home_page, name="home"),
 ]
