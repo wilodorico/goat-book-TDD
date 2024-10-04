b4c784dccf275e055197244d87eacd5107364c13
wilfried <wilfried.odorico@gmail.com>
2024-10-04 21:39:17 +0200

    refacotring

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,4 @@ from django.test import TestCase
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response, "home.html")
